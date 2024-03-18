a57668b7c2c5279909a8d602daa22bb0520261d6
Rob Reeves <roreeves@linkedin.com>
2024-03-18 09:13:34 -0700

    fix import order

--- sql/core/src/main/scala/org/apache/spark/sql/execution/joins/JoinKeyStatsExec.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/joins/JoinKeyStatsExec.scala
@@ -18,10 +18,11 @@
 package org.apache.spark.sql.execution.joins
 
 import scala.collection.mutable
+
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.catalyst.InternalRow
-import org.apache.spark.sql.catalyst.expressions.BindReferences.bindReferences
 import org.apache.spark.sql.catalyst.expressions.{Attribute, Expression}
+import org.apache.spark.sql.catalyst.expressions.BindReferences.bindReferences
 import org.apache.spark.sql.execution.{SparkPlan, UnaryExecNode}
 
 case class JoinKeyStatsExec(child: SparkPlan, keys: Seq[Expression]) extends UnaryExecNode {
